90434e0c20ccc93ee560b9b18609a95e1fb09a70
mehdi <dridignu@gmail.com>
2020-01-11 19:35:18 +0100

    fix docker configurations

--- docker/start-dev.sh
+++ docker/start-dev.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-cd /opt/superdesk/client-core &&
-npm link &&
+
+cd /opt/superdesk/ &&
+python3 -m venv env && . env/bin/activate && pip install -Ur dev-requirements.txt && . activate.sh
+
+#  . /opt/superdesk/activate.sh && python3 manage.py app:flush_elastic_index --capi
+#  . /opt/superdesk/activate.sh && python3 manage.py app:flush_elastic_index --sd
+
 cd /opt/superdesk/client &&
-npm link superdesk-core &&
-npm install &&
 grunt server --server='http://localhost:5000/api' --ws='ws://localhost:5100' &
 
+
+cd /opt/superdesk/client &&
+npm install &&
+grunt build
+
 cd /opt/superdesk &&
+#honcho start
 bash ./scripts/fig_wrapper.sh honcho start
