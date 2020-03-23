6e1107b0183deee176af7f2443a10a898396b3df
Joerg Halitschke <j.halitschke@funkedigital.de>
2020-03-23 15:40:57 +0100

    copy superdesk cloud config after client build, workaround to fix the wring protocol for appConfig.server.url

---
+++ client/superdesk.cloud.config.js
@@ -0,0 +1,9 @@
+window.superdeskConfig={
+    apps: ['superdesk-publisher'],
+    publisher: {
+        protocol: "https",
+        tenant: '',
+        domain: 'publisher.cloud.funkedigital.de',
+        base: 'api/v2'
+    },
+};
