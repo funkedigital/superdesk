7a47290c3a0ea7c6e50490704867096b93c557fc
Joerg Halitschke <j.halitschke@funkedigital.de>
2020-03-24 15:26:43 +0100

    separate client build from start.sh and execute later

---
+++ docker/start-client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /opt/superdesk/client &&
+npm install &&
+grunt build &&
+find ./dist/ -maxdepth 1 -name "config.*.js" -exec cp ./superdesk.cloud.config.js {} \;
+
+#cd /opt/superdesk/client/dist &&
+#sed -i \
+# -e "s/http:\/\/localhost:5000\/api/$(echo $SUPERDESK_URL | sed 's/\//\\\//g')/g" \
+# -e "s/ws:\/\/localhost:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
+# -e "s/ws:\/\/0.0.0.0:5100/$(echo $SUPERDESK_WS_URL | sed 's/\//\\\//g')/g" \
+# -e 's/iframely:{key:""}/iframely:{key:"'$IFRAMELY_KEY'"}/g' \
+# app*.js &&
+nginx &
+
+#cd /opt/superdesk && sleep 5 && bash honcho start
+cd /opt/superdesk && bash ./scripts/fig_wrapper.sh honcho start
