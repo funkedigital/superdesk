6b16aed5c3ae647f734c513313a567abcff3dd04
Joerg Halitschke <j.halitschke@funkedigital.de>
2020-03-31 15:00:04 +0200

    websocket config for supervisor

--- client/superdesk.cloud.config.js
+++ client/superdesk.cloud.config.js
@@ -5,6 +5,7 @@ window.superdeskConfig={
         tenant: '',
         domain: 'publisher.cloud.funkedigital.de',
         wsDomain: 'publisher.cloud.funkedigital.de',
+        wsPath: 'ws',
         wsPort: '8080',
         base: 'api/v2'
     },
