8d1d24b89331141a6f2ba221b038cfc6e7b711bf
Joerg Halitschke <j.halitschke@funkedigital.de>
2020-05-05 13:23:38 +0200

    Moved Mike Lings Funke XMLI Importer from superdesk-core to FD custom project

---
+++ server/fd/io/feeding_services/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk-FD.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+# must be imported for registration
+from fd.io.feeding_services.funke_xmli import FUNKEXMLIFeedingService  # NOQA
