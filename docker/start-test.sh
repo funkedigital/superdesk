1d098805886dfb536a34ec15cbd8359e30831a4c
mehdi <dridignu@gmail.com>
2020-05-29 11:35:32 +0200

    add formatter unit tests

--- docker/start-test.sh
+++ docker/start-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /opt/superdesk/ &&
-python3 -m venv env && . env/bin/activate && pip install -Ur dev-requirements.txt && . activate.sh && nosetests
+python3 -m venv env && . env/bin/activate && pip install -Ur test-requirements.txt && . activate.sh && nosetests
