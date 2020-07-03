e66bf16fb42f52a95413bcfd728f230b2e22e75e
mehdignu <dridignu@gmail.com>
2020-07-03 14:04:34 +0200

    add signal on correction

---
+++ server/fd/rep.py
@@ -0,0 +1,9 @@
+def on_correction(updates, original, **kwargs):
+    sub = []
+    for x in updates.get('subject'):
+        if x['name'] != 'republish':
+            sub.append(x)
+    updates['subject'] = sub
+    
+def init_app(app):
+    app.on_update_archive_correct += on_correction
