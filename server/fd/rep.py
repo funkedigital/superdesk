e85b6ba36778144f127a3b7338659bb7e28551f0
mehdignu <dridignu@gmail.com>
2020-07-07 17:37:02 +0200

    fix the updated iteration

--- server/fd/rep.py
+++ server/fd/rep.py
@@ -1,9 +1,10 @@
 def on_correction(updates, original, **kwargs):
     sub = []
-    for x in updates.get('subject'):
-        if x['name'] != 'republish':
-            sub.append(x)
-    updates['subject'] = sub
+    if updates.get('subject'):
+        for x in updates.get('subject'):
+            if x['name'] != 'republish':
+                sub.append(x)
+        updates['subject'] = sub
     
 def init_app(app):
     app.on_update_archive_correct += on_correction
