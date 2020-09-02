6c10a992f2493904980c67e56a06720611e7c6e7
mehdi dridi <5521531+mehdignu@users.noreply.github.com>
2020-09-02 18:42:29 +0200

    activate venv

--- docker/start-client.sh
+++ docker/start-client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python3 -m venv env && . env/bin/activate && pip install -Ur dev-requirements.txt && . activate.sh &&
+
 # find the config filename and replace it by our sd config, based on environment
 cd /opt/superdesk/client &&
 find ./dist/ -maxdepth 1 -name "config.*.js" -exec cp "/opt/superdesk/client/$SUPERDESK_CLIENT_CONFIG_FILE" {} \;
