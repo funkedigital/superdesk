0bc0d89b6f24645c9b2fa20c417afc09188f9fe4
mehdi dridi <5521531+mehdignu@users.noreply.github.com>
2020-10-13 15:02:49 +0200

    remove the scheduler of the command

--- server/fd/commands/__init__.py
+++ server/fd/commands/__init__.py
@@ -19,17 +19,4 @@ logger = logging.getLogger(__name__)
 
 
 def init_app(app):
-    app.config['CELERY_TASK_ROUTES']['fd.commands.updatecl'] = {
-                'queue': celery_queue('expiry'),
-                'routing_key': 'expiry.updatecl'
-            }
-
-    app.config['CELERY_BEAT_SCHEDULE']['planning:updatecl'] = {
-                'task': 'fd.commands.updatecl',
-                'schedule': timedelta(seconds=600)
-            }
-
-
-@celery.task(soft_time_limit=610)
-def updatecl():
-    UpdateContentLists().run()
+    pass
