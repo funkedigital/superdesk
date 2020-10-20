1e6106c64f5347e70483bf91788248a46d0be790
mehdi <dridignu@gmail.com>
2020-10-20 13:01:28 +0200

    parse spoton metadata

--- server/fd/io/feed_parsers/spoton_parser.py
+++ server/fd/io/feed_parsers/spoton_parser.py
@@ -31,8 +31,72 @@ logger = logging.getLogger(__name__)
 
 
 class SpotonFeedParser(XMLFeedParser):
-    NAME = 'spoton'
-    pass
+    """ Feed Parser for SpotOn """
 
+    NAME = 'spoton'
+
+    label = 'SpotOn Parser'
+
+    def can_parse(self, xml):
+        return xml.tag == 'NewsML'
+
+    def parse(self, xml, provider=None):
+        items = {'associations': {}}
+         
+        try:
+            self.parse_metadata(items, xml)
+            self.parse_content(items, xml)
+            return items
+        except Exception as ex:
+            logger.info(ex)
+    
+    def parse_metadata(self, items, xml):
+        meta_elements = self.parse_elements(xml.find('Meta'))
+
+        author = [{
+                    'name':  meta_elements.get('Author', ''),
+                    'role': 'writer',
+                    'avatar_url': 'https://api.adorable.io/avatars/285/abott@adorable.png'
+            }]
+        items['author'] = author
+        
+        if meta_elements.get('Revision') != None:
+            items['version'] = int(meta_elements.get('Revision'))
+
+        if meta_elements.get('Priority') != None:
+            items['priority'] = int(meta_elements.get('Priority'))
+
+        items['format'] = meta_elements.get('Format', 'html')
+
+        items['type'] = meta_elements.get('Type', 'text')
+
+        keywords_elem = xml.find('Meta/Keywords')
+        keywords = []
+        if len(keywords_elem) > 0:
+            for k in keywords_elem:
+                keywords.append(k.text)
+        items['keywords'] = keywords
+
+        revision_created = meta_elements.get('RevisionCreated', '')
+        if len(revision_created) > 0:
+            items['versioncreated'] = self.datetime(revision_created)
+
+    def parse_content(self, items, xml):
+        pass
+
+
+    def parse_elements(self, tree):
+        parsed = {}
+        for item in tree:
+            # read the value for the items
+            parsed[item.tag] = item.text
+        # remove empty objects
+        parsed = {k: '' if not v else v for k, v in parsed.items()}
+        return parsed
+
+    def datetime(self, string):
+        # Escenic datetime format from CE(S)T
+        local_dt = datetime.datetime.strptime(string, '%a, %d %b %Y %H:%M:%S %z')
+        return local_dt
 
 register_feed_parser(SpotonFeedParser.NAME, SpotonFeedParser())
---
+++ server/fd/tests/io/feed_parsers/spoton_test.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+import datetime
+import os
+import unittest
+import flask
+from pytz import utc
+
+from superdesk.etree import etree
+from fd.io.feed_parsers.spoton_parser import SpotonFeedParser
+from superdesk.io.subjectcodes import init_app as init_subjects
+from superdesk import get_resource_service
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class SpotonParserTest(unittest.TestCase):
+    def setUp(self):
+        app = flask.Flask(__name__)
+        with app.app_context():
+            app.api_prefix = '/api'
+            init_subjects(app)
+            dirname = os.path.dirname(os.path.realpath(__file__))
+            fixture = os.path.normpath(os.path.join(dirname, '../fixtures', 'spoton-feed.xml'))
+            provider = {'name': 'Test'}
+            with open(fixture, 'rb') as f:
+                self.nitf = f.read()
+                self.item = SpotonFeedParser().parse(etree.fromstring(self.nitf), provider)
+
+    def test_parse_metadata(self):
+        self.assertEqual(self.item['author'], [{'name': '(hub/spot)', 'role': 'writer', 'avatar_url': 'https://api.adorable.io/avatars/285/abott@adorable.png'}])
+        self.assertEqual(self.item['version'], 1)
+        self.assertEqual(self.item['priority'], 3)
+        self.assertEqual(self.item['format'], 'STD')
+        self.assertEqual(self.item['type'], 'article')
+        self.assertEqual(self.item['keywords'], ['Christina Stürmer', 'Schwangerschaft', 'Oliver Varga', 'Instagram'])
+        self.assertEqual(self.item['versioncreated'], datetime.datetime.strptime('Tue, 20 Oct 2020 09:39:41 +0000', '%a, %d %b %Y %H:%M:%S %z'))
